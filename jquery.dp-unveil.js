39477c894b41d2dd0c83ee6b66a204d628c682de
michalkutra <michal@kutra.pl>
2016-12-30 11:16:10 +0100

    DP unveil

--- jquery.dp-unveil.js
+++ jquery.dp-unveil.js
@@ -22,7 +22,7 @@
     this.one("unveil", function() {
       var source = this.getAttribute(attrib);
       source = source || this.getAttribute("data-src");
-      if (source) {
+      if (source && !source.match(/<|>/)) {
         this.setAttribute("src", source);
         if (typeof callback === "function") callback.call(this);
       }
